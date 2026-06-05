a871794a998e69627b987f383b36b072c639c855
Prateek Mohapatra 25BCSH77 <cse.25bcsh77@silicon.ac.in>
2026-06-05 20:05:42 +0530

    Update String_Processor.java
    day 10 assignments

--- String_Processor.java
+++ String_Processor.java
@@ -17,3 +17,24 @@ StringIndexOutOfBoundsException
 Example:
 Invalid index handled safely
 */
+package day10_progs;
+import java.util.Scanner;
+public class string_processor {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc =new Scanner(System.in);
+		System.out.print("Enter a String : ");
+		String str=sc.nextLine();
+		try {
+		System.out.println(str.substring(0,5));
+		System.out.println(str.charAt(7));
+		}
+		catch(StringIndexOutOfBoundsException e){
+			System.out.print("Out of index\n");
+		}
+		sc.close();
+		
+	}
+
+}
