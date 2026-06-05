561444d43f113444f6414605e7edd2018acda244
Prateek Mohapatra 25BCSH77 <cse.25bcsh77@silicon.ac.in>
2026-06-05 20:03:55 +0530

    Update ATM_check.java
    day 10 assignments

--- ATM_check.java
+++ ATM_check.java
@@ -11,3 +11,49 @@ If input is invalid (text instead of number) → handle exception
 Expected Output:
 Transaction Successful / Insufficient Balance / Invalid Input
 */
+package day10_progs;
+import java.util.Scanner;
+import java.util.InputMismatchException;//
+
+public class atm_Multilevel_System {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        try {
+            System.out.print("Enter Balance : ");
+            int bal = sc.nextInt();
+
+            System.out.print("Enter Withdrawal Amount : ");
+            int amt = sc.nextInt();
+
+            if (amt < 0) {
+                throw new IllegalArgumentException();
+            }
+
+            if (amt > bal) {
+                throw new ArithmeticException();
+            }
+
+            bal -= amt;
+
+            System.out.println("Transaction Successful");
+            System.out.println("Remaining Balance : " + bal);
+        }
+
+        catch (InputMismatchException e) {
+            System.out.println("Invalid Input");
+        }
+
+        catch (IllegalArgumentException e) {
+            System.out.println("Invalid Input");
+        }
+
+        catch (ArithmeticException e) {
+            System.out.println("Insufficient Balance");
+        }
+
+        sc.close();
+    }
+}
