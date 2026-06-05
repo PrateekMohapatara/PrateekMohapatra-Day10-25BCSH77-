5cbaab5398d41aabf04c8ef9e7080cc7376ebf43
Prateek Mohapatra 25BCSH77 <cse.25bcsh77@silicon.ac.in>
2026-06-05 20:04:37 +0530

    Update Banking_System.java
    day 10 Assignments

--- Banking_System.java
+++ Banking_System.java
@@ -11,3 +11,48 @@ If invalid input → handle exception
 Output:
 Withdrawal successful OR Insufficient balance
 */
+package day10_progs;
+import java.util.InputMismatchException;
+import java .util.Scanner;
+public class banking_system {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		try {
+            System.out.print("Enter Balance : ");
+            int bal = sc.nextInt();
+
+            System.out.print("Enter Withdrawal Amount : ");
+            int amt = sc.nextInt();
+
+            if (amt < 0) {
+                throw new IllegalArgumentException();
+            }
+
+            if (amt > bal) {
+                throw new ArithmeticException();
+            }
+
+            bal -= amt;
+
+            System.out.println("Transaction Successful");
+            System.out.println("Remaining Balance : " + bal);
+        }
+
+        catch (InputMismatchException e) {
+            System.out.println("Invalid Input");
+        }
+
+        catch (IllegalArgumentException e) {
+            System.out.println("Invalid Input");
+        }
+
+        catch (ArithmeticException e) {
+            System.out.println("Insufficient Balance");
+        }
+
+        sc.close();
+	}
+
+}
