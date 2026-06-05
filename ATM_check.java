d4df0ce7ed459b254f501dc2e5ff47196b71961c
rite2harini <rite2harini@gmail.com>
2026-06-05 14:15:46 +0530

    Create ATM_check.java

---
+++ ATM_check.java
@@ -0,0 +1,13 @@
+/*
+ATM Multi-Level Exception System
+
+Create a program that simulates ATM withdrawal.
+
+Requirements:
+Take balance and withdrawal amount
+If withdrawal > balance → handle exception
+If withdrawal amount is negative → handle exception
+If input is invalid (text instead of number) → handle exception
+Expected Output:
+Transaction Successful / Insufficient Balance / Invalid Input
+*/
